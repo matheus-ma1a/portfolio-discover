309a2b5ff7b6952088c0735d2f9528fd109b9684
matheus-ma1a <matheussantosmaia@gmail.com>
2022-02-03 18:18:17 -0300

    cria a funcao que calcula quantos dias foi postado o ultimo repositorio

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ fetch("https://api.github.com/users/matheus-ma1a/repos")
         return resposta.json()
     } )
     .then( (data) => {
-        console.log(data)
+        let resultado = data 
+        console.log(resultado)
+        
+        let dataRepo = resultado[10].created_at
+        let separador = dataRepo.split('T')
+        let date = new Date(`${separador[0]} `)
+        console.log(Math.ceil(Math.abs(date - new Date()) / 86400000))
     })
