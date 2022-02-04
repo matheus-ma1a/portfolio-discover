e7eeab52dd9601e3e58c817ec820eb6721b31a7f
matheus-ma1a <matheussantosmaia@gmail.com>
2022-02-03 22:35:19 -0300

    papula os campos dos cards com informacoes da api github

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-fetch("https://api.github.com/users/matheus-ma1a/repos")
+const card1 = document.querySelector(".nomeRepo")
+const descricao = document.querySelector(".descricao")
+const card2 = document.querySelector(".nomeDoRepoDois")
+const descricaoDois = document.querySelector(".descricao.dois")
+
+
+function pegaDados (){
+
+    fetch("https://api.github.com/users/matheus-ma1a/repos")
     .then( (resposta) => {
         return resposta.json()
     } )
     .then( (data) => {
-        let resultado = data 
-        console.log(resultado)
-        
-        let dataRepo = resultado[10].created_at
-        let separador = dataRepo.split('T')
-        let date = new Date(`${separador[0]} `)
-        console.log(Math.ceil(Math.abs(date - new Date()) / 86400000))
+        card1.innerHTML = data[9].name
+        descricao.innerHTML = data[9].description
+
+        card2.innerHTML = data[8].name
+        descricaoDois.innerHTML = data[8].description
     })
+}
+
+pegaDados()
